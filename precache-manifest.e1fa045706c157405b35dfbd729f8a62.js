00545de11ad24aaa58de9fb322c1da401f7b9b7d
Relise228 <andreysmit43@gmail.com>
2020-08-27 23:38:54 +0300

    Updates

---
+++ precache-manifest.e1fa045706c157405b35dfbd729f8a62.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66202feee69bd09a62d5e646bf1d4120",
+    "url": "/announcement/index.html"
+  },
+  {
+    "revision": "bae202f1ac851883e4ec",
+    "url": "/announcement/static/css/main.b784f24a.chunk.css"
+  },
+  {
+    "revision": "6461823e749182efaa6c",
+    "url": "/announcement/static/js/2.46f284fe.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/announcement/static/js/2.46f284fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bae202f1ac851883e4ec",
+    "url": "/announcement/static/js/main.bca530f1.chunk.js"
+  },
+  {
+    "revision": "93cae4b2addee053f7bb",
+    "url": "/announcement/static/js/runtime-main.866f487b.js"
+  },
+  {
+    "revision": "c55bb4fa0e120cd9bfba7a3d86888eea",
+    "url": "/announcement/static/media/edit.c55bb4fa.svg"
+  },
+  {
+    "revision": "1dca1cc3be8a25d351dc812f24e37413",
+    "url": "/announcement/static/media/trash.1dca1cc3.svg"
+  }
+]);
